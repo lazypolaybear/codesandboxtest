f490a556445635d336396fe174c7ba3d4f1233a5
chi m <ymch.sk2@gmail.com>
2023-04-26 06:31:29 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
+  info abodddut Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
